98c7ea72c3cb761cde6470c22d2902eff7bd2b55
kharman14 <harman.katelyn96@gmail.com>
2023-05-26 06:37:59 -0600

    adding error handling

--- controllers/region.js
+++ controllers/region.js
@@ -2,7 +2,20 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getRegion = async (req, res, next) => {
-  const userId = new ObjectId(req.params.id);
+  const regionName = req.params.regionName;
+  region.find({ regionName: themeName })
+    .then((data) => {
+      if (!data) res.status(404).send({ message: 'Not found region with name: ' + regionName });
+      else res.send(data[0]);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: 'Error retrieving region with regionName=' + regionName,
+        error: err
+      });
+    });
+  
+  /*const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDb()
     .db()
@@ -11,7 +24,7 @@ const getRegion = async (req, res, next) => {
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
-  });
+  });*/
 };
 
 module.exports = { getRegion };
--- controllers/trainers.js
+++ controllers/trainers.js
@@ -2,15 +2,44 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('trainers').find();
+  try {
+    User.find({})
+      .then((data) => {
+        res.status(200).send(data);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || 'Some error occurred while retrieving Trainers.'
+        });
+      });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
+  /*const result = await mongodb.getDb().db().collection('trainers').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
-  });
+  });*/
 };
 
 const getSingle = async (req, res, next) => {
-  const userId = new ObjectId(req.params.id);
+  try {
+    const username = req.params.username;
+    User.find({ username: username })
+      .then((data) => {
+        res.status(200).send(data);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || 'Some error occurred while retrieving Trainer.'
+        });
+      });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
+  /*const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDb()
     .db()
@@ -19,35 +48,96 @@ const getSingle = async (req, res, next) => {
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
-  });
+  });*/
 };
 
 const createTrainer = async (req, res) => {
-  const trainer = {
-    // change to trainer info
+  try {
+    if (!req.body.username || !req.body.password) {
+      res.status(400).send({ message: 'Content can not be empty!' });
+      return;
+    }
+    const password = req.body.password;
+    const passwordCheck = passwordUtil.passwordPass(password);
+    if (passwordCheck.error) {
+      res.status(400).send({ message: passwordCheck.error });
+      return;
+    }
+    const user = new User(req.body);
+    user
+      .save()
+      .then((data) => {
+        console.log(data);
+        res.status(201).send(data);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || 'Some error occurred while creating the Trainer.'
+        });
+      });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+  /*const trainer = {
     username: req.body.username,
     password: req.body.password,
     displayName: req.body.displayName,
     email: req.body.email,
-    info: req.body.info
+    region: req.body.region,
+    pokemon: req.body.pokemon,
+    type: req.body.type
   };
   const response = await mongodb.getDb().db().collection('trainers').insertOne(trainer);
   if (response.acknowledged) {
     res.status(201).json(response);
   } else {
     res.status(500).json(response.error || 'Some error occurred while creating the trainer.');
-  }
+  }*/
 };
 
 const updateTrainer = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
+  try {
+    const username = req.params.username;
+    if (!username) {
+      res.status(400).send({ message: 'Invalid Username Supplied' });
+      return;
+    }
+    const password = req.body.password;
+    const passwordCheck = passwordUtil.passwordPass(password);
+    if (passwordCheck.error) {
+      res.status(400).send({ message: passwordCheck.error });
+      return;
+    }
+    Trainer.findOne({ username: username }, function (err, trainer) {
+      trainer.username = req.params.username;
+      trainer.password = req.body.password;
+      trainer.displayName = req.body.displayName;
+      trainer.email = req.body.email;
+      trainer.region = req.body.region;
+      trainer.pokemon = req.body.pokemon;
+      trainer.type = req.body.type;
+      user.save(function (err) {
+        if (err) {
+          res.status(500).json(err || 'Some error occurred while updating the contact.');
+        } else {
+          res.status(204).send();
+        }
+      });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+  /*const userId = new ObjectId(req.params.id);
   const trainer = {
-    // update to trainer info
     username: req.body.username,
     password: req.body.password,
     displayName: req.body.displayName,
     email: req.body.email,
-    info: req.body.info
+    region: req.body.region,
+    pokemon: req.body.pokemon,
+    type: req.body.type
   };
   const response = await mongodb
     .getDb()
@@ -59,18 +149,35 @@ const updateTrainer = async (req, res) => {
     res.status(204).send();
   } else {
     res.status(500).json(response.error || 'Some error occurred while updating the trainer.');
-  }
+  }*/
 };
 
 const deleteTrainer = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
+  try {
+    const username = req.params.username;
+    if (!username) {
+      res.status(400).send({ message: 'Invalid Username Supplied' });
+      return;
+    }
+    User.deleteOne({ username: username }, function (err, result) {
+      if (err) {
+        res.status(500).json(err || 'Some error occurred while deleting the contact.');
+      } else {
+        res.status(204).send(result);
+      }
+    });
+  } catch (err) {
+    res.status(500).json(err || 'Some error occurred while deleting the contact.');
+  }
+  
+  /*const userId = new ObjectId(req.params.id);
   const response = await mongodb.getDb().db().collection('trainers').deleteOne({ _id: userId }, true);
   console.log(response);
   if (response.deletedCount > 0) {
     res.status(204).send();
   } else {
     res.status(500).json(response.error || 'Some error occurred while deleting the trainer.');
-  }
+  }*/
 };
 
 module.exports = { getAll, getSingle, createTrainer, updateTrainer, deleteTrainer };
