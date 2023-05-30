bc1feaa8c9b41c4c6e14d9a8b79d5bd96d37063b
kharman14 <harman.katelyn96@gmail.com>
2023-05-29 19:56:27 -0600

    Change to get region by region name

--- controllers/region.js
+++ controllers/region.js
@@ -2,15 +2,18 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getRegion = async (req, res, next) => {
-  const userId = new ObjectId(req.params.id);
+  const regionName = req.params.region;
   const result = await mongodb
     .getDb()
     .db()
     .collection('regions')
-    .find({ _id: userId });
+    .find({ regionName: regionName });
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
+  })
+  .catch((err) => {
+    res.status(400).json({ message: err });
   });
 };
 
