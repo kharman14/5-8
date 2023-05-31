7df51ac9703b7f7708a20bb0371c693b6e3b5135
kharman14 <harman.katelyn96@gmail.com>
2023-05-31 12:58:13 -0600

    adding auth0

--- server.js
+++ server.js
@@ -5,6 +5,31 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
 const app = express();
 
+// OAuth
+const { auth, requiresAuth } = require('express-openid-connect');
+require('dotenv').config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+// end of Auth0
+
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
